6549d478cd46b2294c57b96d59207ce26db901a3
Mushtaq A Halepoto <mahalepoto@gmail.com>
2019-02-02 11:57:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,6 +28,5 @@ var funkyFunction = function () {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = "is "+funkyFunction();
-theFunk();
+var theFunk = funkyFunction();
 
